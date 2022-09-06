68accbd80ed205323788cbd36e0977738f004261
hamza0011 <44339149+hamza0011@users.noreply.github.com>
2022-09-06 22:12:55 +0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,11 +4,14 @@ set -e
 
 while true
 do
-  ffmpeg -loglevel info -y -re \
-    -f image2 -loop 1 -i bg.gif \
-    -f concat -safe 0 -i <((for f in ./mp3/*.mp4; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
-    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+  while true
+  do
+    ffmpeg -loglevel info -y -re \
+      -f image2 -loop 1 -i bg.gif \
+      -f concat -safe 0 -i <((for f in ./mp3/*.mp4; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+      -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+      -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+      -c:a aac -b:a 128k -ar 44100 \
+      -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+  done
 done
