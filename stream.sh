81d9198a071bb19c624541d5d58b4aec1e77d6f8
hamza0011 <44339149+hamza0011@users.noreply.github.com>
2022-09-06 21:48:33 +0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -5,7 +5,7 @@ set -e
 while true
 do
   ffmpeg -loglevel info -y -re \
-    -f image2 -loop 1 -i bg.png \
+    -f image2 -loop 1 -i bg.gif \
     -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
